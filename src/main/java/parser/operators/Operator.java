875045c8f6bf88f08fa72beaefe43f7e9e9227ea
nicomeji <Nicolas.X.Mejias.-ND@disney.com>
2015-04-20 18:01:54 -0300

    Create branch

---
+++ src/main/java/parser/operators/Operator.java
@@ -0,0 +1,12 @@
+package parser.operators;
+
+import lombok.Getter;
+
+public class Operator {
+	@Getter
+	private String operator;
+
+	public Operator(String operator) {
+		this.operator = operator;
+	}
+}
