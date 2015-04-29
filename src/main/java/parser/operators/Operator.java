1ec0a2e1477038497328523a33080bad2d6b2a7f
nicomeji <Nicolas.X.Mejias.-ND@disney.com>
2015-04-29 15:15:12 -0300

    Other commit

--- src/main/java/parser/operators/Operator.java
+++ src/main/java/parser/operators/Operator.java
@@ -1,18 +1,39 @@
 package parser.operators;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import parser.SimpleWord;
 import lombok.Getter;
 
 public class Operator {
-    private Pattern pattern;
+    public static final int LONG_STRING = 100;
+
+    private static class SubString {
+        @Getter
+        private final int start;
+        @Getter
+        private final String string;
+
+        private SubString(int start, String string) {
+            this.start = start;
+            this.string = string;
+        }
+
+        public static List<SubString> split(String longString) {
+            List<SubString> subStrings = new ArrayList<Operator.SubString>();
+            if (longString.length() > 100) {
+                int length = longString.length() / 2;
+                subStrings.add(new SubString(0, longString.substring(0, length)));
+                subStrings.add(new SubString(length, longString.substring(length)));
+            } else {
+                subStrings.add(new SubString(0, longString));
+            }
+            return subStrings;
+        }
+    }
+
     @Getter
     private String name;
     @Getter
@@ -32,16 +53,34 @@ public class Operator {
         this.priority = priority;
     }
 
-    public List<Integer> indexesOfOperator(String line) {
-        List<Integer> indexes = Collections.emptyList();
-        if (matcher.find()) {
-            indexes = IntStream.range(0, matcher.groupCount()).boxed().map(index -> {
-                return matcher.group(index).indexOf(symbol);
-            }).collect(Collectors.toList());
-            if (indexes.isEmpty()) {
-                indexes = Arrays.asList(line.indexOf(symbol));
-            }
+    private List<Integer> indexesForLongString(String line) {
+        List<Integer> indexes = Collections.synchronizedList(new ArrayList<Integer>());
+        SubString.split(line).parallelStream().forEach(subString -> {
+            indexes.addAll(indexes(subString.getString()).stream().map(index -> {
+                return index + subString.getStart();
+            }).collect(Collectors.toList()));
+        });
+        return indexes;
+    }
+
+    private List<Integer> indexesForShortString(String line) {
+        List<Integer> indexes = new ArrayList<Integer>();
+        int actualIndex = line.indexOf(symbol), lastIndex = 0;
+        while (actualIndex >= 0) {
+            indexes.add(actualIndex + lastIndex);
+            lastIndex += (actualIndex + 1);
+            actualIndex = (line = line.substring(actualIndex + 1)).indexOf(symbol);
         }
         return indexes;
+    }
+
+    public List<Integer> indexes(String line) {
+        List<Integer> returnedList;
+        if (line.length() > LONG_STRING) {
+            returnedList = indexesForLongString(line);
+        } else {
+            returnedList = indexesForShortString(line);
+        }
+        return returnedList.stream().sorted().collect(Collectors.toList());
     }
 }
--- src/test/java/parser/operators/OperatorTest.java
+++ src/test/java/parser/operators/OperatorTest.java
@@ -1,14 +1,17 @@
 package parser.operators;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
 import javax.management.BadAttributeValueExpException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 
 public class OperatorTest {
     private static final String NAME = "Operator";
@@ -37,23 +40,38 @@ public class OperatorTest {
     }
 
     @Test
-    public void operatorsHasMatcher_1() {
+    public void operatorsHasMatcher() {
         String testSentence = "number=1;";
-        List<Integer> indexes = operator.indexesOfOperator(testSentence);
+        List<Integer> indexes = operator.indexes(testSentence);
         assertThat(indexes, contains(6));
     }
 
     @Test
-    public void operatorsHasMatcher_2() {
+    public void operatorsHasMatcher_1() {
         String testSentence = "number1=number2;";
-        List<Integer> indexes = operator.indexesOfOperator(testSentence);
+        List<Integer> indexes = operator.indexes(testSentence);
         assertThat(indexes, contains(7));
     }
 
     @Test
-    public void operatorsHasMatcher_3() {
-        String testSentence = "number1=number2=0;";
-        List<Integer> indexes = operator.indexesOfOperator(testSentence);
-        assertThat(indexes, contains(7,15));
+    public void operatorsHasMatcher_2() {
+        String testSentence = "number1=number2 =   0;";
+        List<Integer> indexes = operator.indexes(testSentence);
+        assertThat(indexes, contains(7, 16));
+    }
+
+    @Test
+    public void operatorsProcessLongString() {
+        String testSentence = "number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number=number";
+        List<Integer> indexes = operator.indexes(testSentence);
+        assertThat(testSentence.length(), is(greaterThan(Operator.LONG_STRING)));
+        assertThat(
+                indexes,
+                contains(6, 13, 20, 27, 34, 41, 48, 55, 62, 69, 76, 83, 90, 97, 104, 111, 118, 125, 132, 139, 146, 153,
+                        160, 167, 174, 181, 188, 195, 202, 209, 216, 223, 230, 237, 244, 251, 258, 265, 272, 279, 286,
+                        293, 300, 307, 314, 321, 328, 335, 342, 349, 356, 363, 370, 377, 384, 391, 398, 405, 412, 419,
+                        426, 433, 440, 447, 454, 461, 468, 475, 482, 489, 496, 503, 510, 517, 524, 531, 538, 545, 552,
+                        559, 566, 573, 580, 587, 594, 601, 608, 615, 622, 629, 636, 643, 650, 657, 664, 671, 678, 685,
+                        692, 699, 706, 713, 720, 727, 734, 741, 748, 755, 762));
     }
 }
