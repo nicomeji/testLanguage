2c084abb5fce7e91451b035d92a00ef91c0486cb
Nicolas Mejias <Nicolas.X.Mejias.-ND@disney.com>
2015-08-13 17:29:18 -0300

    Partial commit

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -1,33 +1,52 @@
 package parser;
 
 import java.io.FileNotFoundException;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.ResourceBundle;
+//import java.util.stream.Collectors;
 
-import org.apache.commons.cli.ParseException;
+
+
+
+
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.support.ResourceBundleMessageSource;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import com.beust.jcommander.JCommander;
 
 import parser.cmdline.Arguments;
-import parser.cmdline.ArgumentsProcessor;
+import parser.cmdline.OptionsDescriptions;
 
 public class Main implements Runnable {
-    private final Arguments arguments;
-    private final Parser parser;
+//    private final Parser parser;
 
-    public static void main(String[] args) throws ParseException, FileNotFoundException {
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"CommandLineParserContext.xml"});
+    public static void main(String[] args) throws IOException {
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"CommandLineParserContext.xml"});
+        ApplicationContext context = null;
+        Resource resource = new ClassPathResource("/my.properties");
+        ResourceBundle descriptions = new OptionsDescriptions(PropertiesLoaderUtils.loadProperties(resource));
+        
         Main program = new Main(args, context);
         program.run();
     }
 
-    public Main(String[] args, ApplicationContext applicationContext) throws ParseException, FileNotFoundException {
-        ArgumentsProcessor cmdParser = (ArgumentsProcessor) applicationContext.getBean("cmdlineProcessor");
-        this.arguments = cmdParser.parseArgs(args);
-        this.parser = new Parser();
+    public Main(String[] args, ApplicationContext applicationContext) throws FileNotFoundException {
+//        ResourceBundleMessageSource resourceBundle = (ResourceBundleMessageSource) applicationContext.getBean("messages");
+        Arguments arguments = new Arguments();
+        JCommander cmd = new JCommander(arguments, ResourceBundle.getBundle("lineParserDescriptions"));
+        cmd.parse(args);
+        System.out.println(arguments);
+//        this.parser = new Parser();
     }
 
     @Override
     public void run() {
-        arguments.getSources().stream().parallel().map(parser::parse).collect(Collectors.toList());
+//        arguments.getSources().stream().parallel().map(parser::parse).collect(Collectors.toList());
     }
 }
--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -3,7 +3,8 @@ package parser;
 import java.io.FileInputStream;
 
 public class Parser {
-    public Object parse(FileInputStream f) {
+//    public Object parse(FileInputStream f) {
+        public Object parse(String f) {
         return null;
     }
 }
