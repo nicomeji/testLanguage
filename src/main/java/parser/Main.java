a3bbba6359e98f4f2eea5a0a064228b59198b671
Nicolas Mejias <Nicolas.X.Mejias.-ND@disney.com>
2015-08-14 14:54:51 -0300

    Reverting

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -1,52 +1,64 @@
 package parser;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ResourceBundle;
-//import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-
-
-
-
-
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.springframework.context.ApplicationContext;
-//import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import org.springframework.context.support.ResourceBundleMessageSource;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-
-import com.beust.jcommander.JCommander;
-
-import parser.cmdline.Arguments;
-import parser.cmdline.OptionsDescriptions;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main implements Runnable {
-//    private final Parser parser;
+    private final List<FileInputStream> sourceFiles;
+    private final Parser parser;
+    private final CommandLineParser cmdParser;
 
-    public static void main(String[] args) throws IOException {
-//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"CommandLineParserContext.xml"});
-        ApplicationContext context = null;
-        Resource resource = new ClassPathResource("/my.properties");
-        ResourceBundle descriptions = new OptionsDescriptions(PropertiesLoaderUtils.loadProperties(resource));
-        
+    public static void main(String[] args) throws ParseException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Main program = new Main(args, context);
         program.run();
     }
 
-    public Main(String[] args, ApplicationContext applicationContext) throws FileNotFoundException {
-//        ResourceBundleMessageSource resourceBundle = (ResourceBundleMessageSource) applicationContext.getBean("messages");
-        Arguments arguments = new Arguments();
-        JCommander cmd = new JCommander(arguments, ResourceBundle.getBundle("lineParserDescriptions"));
-        cmd.parse(args);
-        System.out.println(arguments);
-//        this.parser = new Parser();
+    public Main(String[] args, ApplicationContext applicationContext) throws ParseException {
+        Option option = new Option("f", "file", true, "Source file to be parsed.");
+        Options options = new Options();
+        options.addOption(option);
+        CommandLine cmd = new PosixParser().parse(options, args);
+
+        sourceFiles = getSources(cmd).stream().map(this::checkFiles).collect(Collectors.toList());
+        this.cmdParser = null;
+        this.parser = null;
+    }
+
+    private List<File> getSources(CommandLine cmd) throws ParseException {
+        List<String> sources = Arrays.asList(cmd.getArgs());
+        if (cmd.hasOption("f") && sources.isEmpty()) {
+            sources = Arrays.asList(cmd.getOptionValues("f"));
+        } else if (sources.isEmpty()) {
+            throw new ParseException("");
+        }
+        return sources.stream().map(File::new).collect(Collectors.toList());
+    }
+
+    private FileInputStream checkFiles(File f) {
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return stream;
     }
 
     @Override
     public void run() {
-//        arguments.getSources().stream().parallel().map(parser::parse).collect(Collectors.toList());
+        sourceFiles.stream().parallel().map(parser::parse).collect(Collectors.toList());
     }
 }
