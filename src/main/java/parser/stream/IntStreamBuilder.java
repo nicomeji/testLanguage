1d74141378d3b99f32a716086a06b27ec016ce68
nicomeji <nico_meji@yahoo.com.ar>
2016-03-11 18:14:21 -0300

    Fixing Character Stream

--- src/main/java/parser/stream/IntStreamBuilder.java
+++ src/main/java/parser/stream/IntStreamBuilder.java
@@ -1,28 +1,14 @@
 package parser.stream;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import lombok.Getter;
 
 @Getter
 public class IntStreamBuilder {
-    private Stream<Character> charStream = Stream.empty();
-
-    private IntStreamBuilder(Stream<String> in) {
-        in.map(String::toCharArray).map(charArray -> {
-            return Arrays.stream(charArray);
-        }).forEach(string -> {
-            charStream = Stream.concat(charStream, string.map(this::intToChar));
-        });
-    }
-
-    private Character intToChar (int a) {
-        return (char) a;
-    }
-
-    public static IntStream of(Stream<String> in) {
-        return new IntStreamBuilder(in).getCharStream().map(value-> new Character(Character.toChars(value)[0]));
-    }
+	public static Stream<Character> of(Stream<String> in) {
+		return in.flatMap(string -> {
+			return string.chars().mapToObj(character -> (char) character).map(Character::new);
+		});
+	}
 }
--- src/test/java/parser/stream/IntStreamBuilderTest.java
+++ src/test/java/parser/stream/IntStreamBuilderTest.java
@@ -5,8 +5,7 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.util.PrimitiveIterator.OfInt;
-import java.util.stream.IntStream;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -15,17 +14,17 @@ public class IntStreamBuilderTest {
     @Test
     public void testParsingString () {
         Stream<String> strings = Stream.of("Hello world");
-        IntStream stream = IntStreamBuilder.of(strings);
+        Stream<Character> stream = IntStreamBuilder.of(strings);
         assertThat(stream, is(not(nullValue())));
         assertThat(stream.count(), is(11L));
     }
 
     @Test
     public void testCharactersOrder () {
-        Stream<String> strings = Stream.of("12", "45");
-        IntStream stream = IntStreamBuilder.of(strings);
+        Stream<String> strings = Stream.of("12", "3", "45");
+        Stream<Character> stream = IntStreamBuilder.of(strings);
         assertThat(stream, is(not(nullValue())));
-        OfInt iterator = stream.iterator();
+        Iterator<Character> iterator = stream.iterator();
         assertThat(iterator.next(), is('1'));
         assertThat(iterator.next(), is('2'));
         assertThat(iterator.next(), is('3'));
