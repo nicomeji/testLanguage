34f6b24b605a2747ebecd1291aba92a4c8279100
nicomeji <nico_meji@yahoo.com.ar>
2016-03-25 14:15:24 -0300

    Refactor

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -6,12 +6,13 @@ import java.nio.file.Files;
 import java.util.stream.Stream;
 
 import parser.context.Context;
-import parser.stream.IntStreamBuilder;
 
 public class Parser {
     public Context parse(File f) {
         try (Stream<String> lines = Files.lines(f.toPath())) {
-            Stream<Character> iterator = IntStreamBuilder.of(lines);
+            Stream<Character> iterator=lines.flatMap(string -> {
+                return string.chars().mapToObj(character -> (char) character).map(Character::new);
+            });
         } catch (IOException e) {
             throw new IllegalArgumentException("Problem with file: " + f, e.getCause());
         }
