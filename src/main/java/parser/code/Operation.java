6c0b68f2a469074bb5da3f3dcf20e6bcc3986511
nicomeji <nico_meji@yahoo.com.ar>
2016-04-28 08:57:45 -0300

    Need to check this

---
+++ src/main/java/parser/code/Operation.java
@@ -0,0 +1,10 @@
+package parser.code;
+
+import java.util.List;
+
+import parser.operators.Operator;
+
+public class Operation {
+    private Operator operator;
+    private List<String> operands;
+}
